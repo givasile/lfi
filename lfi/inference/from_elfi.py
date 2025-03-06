49dc7a91d71edc223a67eb9347a900e1a1fcfe77
Theodora Chrysoula <75185540+TheodoraChrysoula@users.noreply.github.com>
2025-03-06 12:00:24 +0200

    SMC_ELFI

--- lfi/inference/from_elfi.py
+++ lfi/inference/from_elfi.py
@@ -27,3 +27,34 @@ class RejectionSampling(InferenceBase):
         return samples_res.samples_array
 
 
+class SMCRejection(InferenceBase):
+    def __init__(self, prior, simulator, observation):
+        elfi_sim = simulator.return_elfi_callable()
+
+        thetas = prior.return_elfi_objects()
+        self.sim = elfi.Simulator(elfi_sim, *thetas, observed=observation)
+        self.d = elfi.Distance('euclidean', self.sim)
+        dim = len(thetas)
+        dim_y = observation.shape[0]
+        super().__init__('elfi_smc_rejection', thetas, self.sim, observation, dim, dim_y)
+
+    def fit(self, budget: int=1000, fit_kwargs: dict=None):
+        self.budget = budget
+        default_kwargs = {
+            "batch_size": 1_000
+
+        }
+        default_kwargs.update((fit_kwargs or {}))
+        self.inference_method = elfi.SMC(self.d, batch_size=default_kwargs["batch_size"] )
+
+    def sample(self, nof_samples: int=100, sample_kwargs: dict=None):
+        default_kwargs = {
+            "nof_iterations": 4
+            }
+        default_kwargs.update((sample_kwargs or {}))
+        quantiles = [1 - i / default_kwargs["nof_iterations"] 
+                        for i in range(default_kwargs["nof_iterations"])]
+
+        default_kwargs.update((sample_kwargs or {}))
+        samples_res = self.inference_method.sample(nof_samples, quantiles=quantiles)
+        return samples_res.samples_array
