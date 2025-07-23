d15d0fdcc743403f35734e911968ebf06c04a4f7
Vasilis Gkolemis <vasilis.gkolemis@gmail.com>
2025-07-23 11:10:54 +0300

    concept image

--- scripts/concept_figure/distractors.py
+++ scripts/concept_figure/distractors.py
@@ -7,6 +7,13 @@ import exp_utils
 # Global config
 # ----------------------------- #
 
+budget_list = [10_000, 30_000]
+r2omc = False
+npe = True
+snpe = True
+bayes_flow = True
+flow_matching = True
+
 np.random.seed(42)
 dir_path = "./../../paper/figures/concept_figure/distractors"
 os.makedirs(dir_path, exist_ok=True)
@@ -18,8 +25,6 @@ dim_distractors = 18
 dim_y = 2 + dim_distractors
 nof_observations = 1
 nof_samples = 1000
-
-budget_list = [1_000, 10_000, 30_000]
 
 # ----------------------------- #
 # Problem setup
@@ -53,86 +58,91 @@ samples_gt = gt_posterior.sample(1000)
 exp_utils.plot_samples(dir_path, None, samples_gt, "gt", title=None, runtime=None)
 
 # ROMC
-c2st, runtime, samples = exp_utils.run_inference(
-    prior=prior,
-    simulator=simulator,
-    observation=observation,
-    samples_gt=samples_gt,
-    method_name="r2omc_1000",
-    inference_class=lfi.inference.r2omc.R2OMC,
-    budget=1000,
-    nof_samples=nof_samples,
-    fit_kwargs={"pcg_to_keep": 1., "box_algorithm": "standard", "dx": 0.2},
-    sample_kwargs={"samples_per_region": 2},
-)
-exp_utils.save_stats(dir_path, samples, samples_gt, "r2omc_1000", None, c2st, runtime)
+if r2omc:
+    c2st, runtime, samples = exp_utils.run_inference(
+        prior=prior,
+        simulator=simulator,
+        observation=observation,
+        samples_gt=samples_gt,
+        method_name="r2omc_1000",
+        inference_class=lfi.inference.r2omc.R2OMC,
+        budget=1000,
+        nof_samples=nof_samples,
+        fit_kwargs={"pcg_to_keep": 1., "box_algorithm": "standard", "dx": 0.2},
+        sample_kwargs={"samples_per_region": 2},
+    )
+    exp_utils.save_stats(dir_path, samples, samples_gt, "r2omc_1000", None, c2st, runtime)
 
 # NPE
-for budget in budget_list:
-    c2st, runtime, samples = exp_utils.run_inference(
-        prior=prior,
-        simulator=simulator,
-        observation=observation,
-        samples_gt=samples_gt,
-        method_name=f"npec_{budget}",
-        inference_class=lfi.inference.from_sbi.NPECSingleRound,
-        budget=budget,
-        nof_samples=nof_samples,
-        fit_kwargs={"batch_size": 100, "training_batch_size": 100},
-        sample_kwargs=None,
-    )
-    exp_utils.save_stats(dir_path, samples, samples_gt, f"npec_{budget}", None, c2st, runtime)
+if npe:
+    for budget in budget_list:
+        c2st, runtime, samples = exp_utils.run_inference(
+            prior=prior,
+            simulator=simulator,
+            observation=observation,
+            samples_gt=samples_gt,
+            method_name=f"npec_{budget}",
+            inference_class=lfi.inference.from_sbi.NPECSingleRound,
+            budget=budget,
+            nof_samples=nof_samples,
+            fit_kwargs={"batch_size": 100, "training_batch_size": 100},
+            sample_kwargs=None,
+        )
+        exp_utils.save_stats(dir_path, samples, samples_gt, f"npec_{budget}", None, c2st, runtime)
 
 # S-NPE
-for budget in budget_list:
-    c2st, runtime, samples = exp_utils.run_inference(
-        prior=prior,
-        simulator=simulator,
-        observation=observation,
-        samples_gt=samples_gt,
-        method_name=f"snpec_{budget}",
-        inference_class=lfi.inference.from_sbi.NPECMultiRound,
-        budget=budget,
-        nof_samples=nof_samples,
-        fit_kwargs={"batch_size": 100, "training_batch_size": 100, "num_rounds": 3},
-        sample_kwargs=None,
-    )
-    exp_utils.save_stats(dir_path, samples, samples_gt, f"snpec_{budget}", None, c2st, runtime)
+if snpe:
+    for budget in budget_list:
+        c2st, runtime, samples = exp_utils.run_inference(
+            prior=prior,
+            simulator=simulator,
+            observation=observation,
+            samples_gt=samples_gt,
+            method_name=f"snpec_{budget}",
+            inference_class=lfi.inference.from_sbi.NPECMultiRound,
+            budget=budget,
+            nof_samples=nof_samples,
+            fit_kwargs={"batch_size": 100, "training_batch_size": 100, "num_rounds": 3},
+            sample_kwargs=None,
+        )
+        exp_utils.save_stats(dir_path, samples, samples_gt, f"snpec_{budget}", None, c2st, runtime)
 
 # Bayes Flow
-for budget in budget_list:
-    c2st, runtime, samples = exp_utils.run_inference(
-        prior=prior,
-        simulator=simulator,
-        observation=observation,
-        samples_gt=samples_gt,
-        method_name=f"bayes_flow_{budget}",
-        inference_class=lfi.inference.from_sbi.BayesFlow,
-        budget=budget,
-        nof_samples=nof_samples,
-        fit_kwargs={
-            "batch_size": 100,
-            "training_batch_size": 100,
-            "embedding_net_output_dim": 2,
-            "embedding_net_num_layers": 1,
-            "embedding_net_num_hiddens": 32,
-        },
-        sample_kwargs=None,
-    )
-    exp_utils.save_stats(dir_path, samples, samples_gt, f"bayes_flow_{budget}", None, c2st, runtime)
+if bayes_flow:
+    for budget in budget_list:
+        c2st, runtime, samples = exp_utils.run_inference(
+            prior=prior,
+            simulator=simulator,
+            observation=observation,
+            samples_gt=samples_gt,
+            method_name=f"bayes_flow_{budget}",
+            inference_class=lfi.inference.from_sbi.BayesFlow,
+            budget=budget,
+            nof_samples=nof_samples,
+            fit_kwargs={
+                "batch_size": 100,
+                "training_batch_size": 100,
+                "embedding_net_output_dim": 2,
+                "embedding_net_num_layers": 1,
+                "embedding_net_num_hiddens": 32,
+            },
+            sample_kwargs=None,
+        )
+        exp_utils.save_stats(dir_path, samples, samples_gt, f"bayes_flow_{budget}", None, c2st, runtime)
 
 # Flow Matching
-for budget in budget_list:
-    c2st, runtime, samples = exp_utils.run_inference(
-        prior=prior,
-        simulator=simulator,
-        observation=observation,
-        samples_gt=samples_gt,
-        method_name=f"flow_matching_{budget}",
-        inference_class=lfi.inference.from_sbi.FMPESingleRound,
-        budget=budget,
-        nof_samples=nof_samples,
-        fit_kwargs={"batch_size": 100, "training_batch_size": 100},
-        sample_kwargs=None,
-    )
-    exp_utils.save_stats(dir_path, samples, samples_gt, f"flow_matching_{budget}", None, c2st, runtime)
+if flow_matching:
+    for budget in budget_list:
+        c2st, runtime, samples = exp_utils.run_inference(
+            prior=prior,
+            simulator=simulator,
+            observation=observation,
+            samples_gt=samples_gt,
+            method_name=f"flow_matching_{budget}",
+            inference_class=lfi.inference.from_sbi.FMPESingleRound,
+            budget=budget,
+            nof_samples=nof_samples,
+            fit_kwargs={"batch_size": 100, "training_batch_size": 100},
+            sample_kwargs=None,
+        )
+        exp_utils.save_stats(dir_path, samples, samples_gt, f"flow_matching_{budget}", None, c2st, runtime)
